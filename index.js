1a9fa32773e0e0d0aae257c1908aa5dbe2fea977
frankmuthomi30 <frankmuthomi30@gmail.com>
2023-02-24 10:34:54 +0300

    sina jambo

---
+++ index.js
@@ -0,0 +1,15 @@
+const numbers = [18, 52, 36, 47, 54];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  sum = sum + numbers[i];
+}
+
+console.log(sum); 
+
+let s = ['lorem at'];
+let reversed = s[0].split('').reverse().join('');
+let newString = reversed.slice(0, 8);
+console.log(newString); // Output: "merol"
+
+// Constructive criticism- refers to feedback or comments that are intended to help someone improve their work, behavior, or performance.
